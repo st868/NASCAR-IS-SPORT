091174cfa7570c74873634c9e25bb14e07d0a361
st868 <59935377+st868@users.noreply.github.com>
2020-03-05 09:40:05 -0500

    Working on home + attempt at some js/page transition.

---
+++ dev/js/point2.js
@@ -0,0 +1,37 @@
+// const flip2 = document.getElementsByTagName("whyisnascarasport2");
+// //
+// for (const button of flip2) {
+//   button.addEventListener('click', () => {
+//      var id = button.getAttribute("id");
+
+     // var layerClass = "." + id+ "-layer";
+     // var layers = document.querySelectorAll(layerClass);
+     // for (const layer of layers) {
+     //   layer.classList.toggle("active");
+     // }
+//   });
+// }
+
+
+
+// $( "#whyisnascarasport2" ).click(function() {
+//   // $( "#target" ).mousedown(); var layerClass = "." + id+ "-layer";
+//    var layers = document.querySelectorAll(layerClass);
+//    for (const layer of layers) {
+//      layer.classList.toggle("active");
+//    }
+// });
+
+// const buttons = document.getElementsByTagName("button");
+//
+// for (const button of buttons) {
+//   button.addEventListener('click', () => {
+//      var id = button.getAttribute("id");
+//
+//      var layerClass = "." + id+ "-layer";
+//      var layers = document.querySelectorAll(layerClass);
+//      for (const layer of layers) {
+//        layer.classList.toggle("active");
+//      }
+//   });
+// }
