61706840993d6f3ce720677c11a097e02b8e9baf
st868 <59935377+st868@users.noreply.github.com>
2020-03-09 22:48:25 -0400

    Updated the master file with the beginnings of Waypoints

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,10 @@
+// @codekit-prepend "nav.js";
+// @codekit-prepend "burger/burger.js";
+// @codekit-prepend "burger/burgerexit.js";
+// @codekit-prepend "burger/burgerenter.js";
+// @codekit-prepend "burger/burgerclick.js";
+// @codekit-prepend "waypoints.js";
 
+$( document ).ready(function() {
+    gsap.registerPlugin(ScrollToPlugin);
+});
---
+++ dev/js/waypoints.js
@@ -0,0 +1,35 @@
+var inviewQA = new Waypoint.Inview({
+  element: $('.QAcontainer')[0],
+  enter: function(direction) {
+     headerColor(direction, scrollDirection = "enter");
+  },
+  entered: function(direction) {
+        headerColor(direction , scrollDirection = "entered");
+  },
+  exit: function(direction) {
+        headerColor(direction , scrollDirection = "exit");
+  },
+  exited: function(direction) {
+    headerColor(direction , scrollDirection = "exited");
+  },offset: {
+    top: 50 // 67px
+  }
+})
+
+
+function headerColor(direction, scrollDirection){
+
+    if(direction === "up" & scrollDirection === "enter"){
+        gsap.to("#pageNav",{duration:0.5,backgroundColor:"#000"});
+
+    }else if(direction === "up"& scrollDirection === "entered"){
+        gsap.to("#pageNav",{duration:0.5,backgroundColor:"#fff"});
+
+    }else if(direction === "down"& scrollDirection === "exit"){
+        gsap.to("#pageNav",{duration:0.5,backgroundColor:"#000"});
+
+    } else if(direction === "down"& scrollDirection === "exited"){
+        gsap.to("#pageNav",{duration:0.5,backgroundColor:"#fff"});
+    }
+
+}
